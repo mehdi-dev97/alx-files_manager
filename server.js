29dd0a7ee4ad64b5d856868d0198dcbbb9b4ff02
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2024-05-15 11:33:04 +0100

    create a file db.js that contains the class DBClient

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log(process.env.IP_ADDRESS);
+import express from 'express';
+import startServer from './libs/boot';
+import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
+
+const server = express();
+
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
+
+export default server;
